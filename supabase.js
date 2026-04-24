4ecac24c01d500b60f31e68afc3aa9631a325bd5
vavavbabano-svg <ksioneru@gmail.com>
2026-04-24 23:44:52 +0300

    soften buy button color for better contrast and UI balance

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://YOUR_PROJECT.supabase.co";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://naxxslgxyelefzdxjhze.supabase.co";
+const supabaseKey = "sb_publishable_cU_zUkI5f_qltx0KQIe6xw_k4JLk-IF";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
