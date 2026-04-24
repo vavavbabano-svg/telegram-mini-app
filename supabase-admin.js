29369b6a73f9bd28f06bbabee9104c2887c25e75
vavavbabano-svg <ksioneru@gmail.com>
2026-04-25 01:30:05 +0300

    remove real key from code

--- supabase-admin.js
+++ supabase-admin.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://naxxslgxyelefzdxjhze.supabase.co";
-const supabaseKey = "sb_secret_dBNtz9mCMKrwnoq2hg46Ww_Rw8J_fWZ"; // ← возьми из Supabase → Project Settings → API → service_role
+const supabaseKey = ""; // ← возьми из Supabase → Project Settings → API → service_role
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
