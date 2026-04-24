ec73f20a6da4a638c0cf6ab30a940f0c86481da4
vavavbabano-svg <ksioneru@gmail.com>
2026-04-24 17:02:34 +0300

    пакеты

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ function setStars(val) {
   starsInput.value = val;
   update();
 }
+function adjustPacks(amount) {
+  let packValueElement = document.getElementById('packValue');
+  let currentValue = parseInt(packValueElement.innerText, 10);
+  let newValue = currentValue + amount;
+
+  // Ограничим минимальное и максимальное значение
+  if (newValue < 50) newValue = 50;
+  if (newValue > 5000) newValue = 5000;
+
+  packValueElement.innerText = newValue;
+  // Обновляем количество звёзд в input
+  document.getElementById('stars').value = newValue;
+  update(); // обновляем расчёт рублёй
+}
