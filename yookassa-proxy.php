cbdb745ae9ffc159cb510c0baf8a7406d9a6aa49
vavavbabano-svg <ksioneru@gmail.com>
2026-04-27 16:36:52 +0300

    update

---
+++ yookassa-proxy.php
@@ -0,0 +1,39 @@
+<?php
+// yookassa-proxy.php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
+}
+
+$shopId = '1343358';
+$secretKey = 'test_NjodJO1Gkl9oRh7mCQNmPV0-p7T9ekDH4fBXDlPWR4M';
+
+$input = file_get_contents('php://input');
+$paymentData = json_decode($input, true);
+
+if (!$paymentData) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Invalid JSON']);
+    exit;
+}
+
+$auth = base64_encode($shopId . ':' . $secretKey);
+
+$ch = curl_init('https://api.yookassa.ru/v3/payments');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Authorization: Basic ' . $auth
+]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
