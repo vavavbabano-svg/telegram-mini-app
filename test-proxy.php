f635e5535179ecab59e8b1568ccf302b509c160b
vavavbabano-svg <ksioneru@gmail.com>
2026-04-27 18:02:32 +0300

    fix: remove duplicate event listener and extra brace in payment.html

--- test-proxy.php
+++ test-proxy.php
@@ -1,8 +1,7 @@
 <?php
-// Простой тестовый скрипт: всегда возвращает JSON
 header('Content-Type: application/json');
 echo json_encode([
     'status' => 'ok',
-    'message' => 'Прокси работает, PHP в порядке',
+    'message' => 'Прокси работает',
     'time' => date('Y-m-d H:i:s')
 ]);
